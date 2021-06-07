9ee1c5c668d927c212384d173d0dbe9303867c44
Ali Abbas Kanadia <aakanadia@gmail.com>
2021-06-07 12:38:36 +0530

    Get Room Functionality

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
-import { BrowserRouter as Router, Switch, Route, Link, Redirect }  from "react-router-dom"; 
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import Room from "./Room";
 
 
 export default class HomePage extends Component {
@@ -17,7 +18,8 @@ export default class HomePage extends Component {
                         <p>This is the home page.</p>
                     </Route>
                     <Route path="/join" component={RoomJoinPage}/>
-                    <Route path="/create" component={CreateRoomPage}/>
+                    <Route path="/create" component={CreateRoomPage} />
+                    <Route path="/room/:roomCode" component={Room} />
                 </Switch>
             </Router>
         );
