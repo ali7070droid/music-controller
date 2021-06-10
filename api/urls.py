4de0ca72b93f12c660b2feea1bf07acd32ad21e6
Ali Abbas Kanadia <aakanadia@gmail.com>
2021-06-11 00:32:10 +0530

    django sessions and room management

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from .models import Room
 from django.urls import path
-from .views import CreateRoomView, RoomView, GetRoom
+from .views import CreateRoomView, RoomView, GetRoom, JoinRoom, UserInRoom,LeaveRoom
 urlpatterns = [
     path('room',RoomView.as_view()),
     path('create-room',CreateRoomView.as_view()),
-    path('get-room', GetRoom.as_view())
+    path('get-room', GetRoom.as_view()),
+    path('join-room',JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view()),
+    path('leave-room',LeaveRoom.as_view())
 ]
