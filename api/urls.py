5bed7ad60709ba36cb89390cb6a176b1a0944643
Ali Abbas Kanadia <aakanadia@gmail.com>
2021-06-11 18:14:55 +0530

    update part with callback

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
+from music_controller.api.serializers import UpdateRoomSerializer
 from .models import Room
 from django.urls import path
-from .views import CreateRoomView, RoomView, GetRoom, JoinRoom, UserInRoom,LeaveRoom
+from .views import CreateRoomView, RoomView, GetRoom, JoinRoom, UserInRoom,LeaveRoom, UpadteRoom
 urlpatterns = [
     path('room',RoomView.as_view()),
     path('create-room',CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
     path('join-room',JoinRoom.as_view()),
     path('user-in-room', UserInRoom.as_view()),
-    path('leave-room',LeaveRoom.as_view())
+    path('leave-room',LeaveRoom.as_view()),
+    path('update-room',UpadteRoom.as_view())
 ]
